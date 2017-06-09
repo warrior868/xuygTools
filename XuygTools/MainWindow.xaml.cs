173dad8396c28458d73d1c8142685011aea7d793
Administrator <Administrator@WINDOWS-UHLF50G>
2017-06-09 23:56:37 +0800

    1.增加可配置文件；

--- XuygTools/MainWindow.xaml.cs
+++ XuygTools/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
-
+using System.IO;
 
 namespace XuygTools
 {
@@ -26,15 +26,16 @@ namespace XuygTools
         {
             InitializeComponent();
             this.DragArea.MouseDown += new MouseButtonEventHandler(DragArea_mouseDown); //鼠标移动拖动窗口位置
-            this.exitButton.MouseDown +=new MouseButtonEventHandler(exitButton_Click); //关闭程序
+            this.exitButton.MouseDown += new MouseButtonEventHandler(exitButton_Click); //关闭程序
             this.getPathBUT.MouseDown += new MouseButtonEventHandler(getPathBUT_Click); //关闭程序
+            this.createFilesBUT.MouseDown += new MouseButtonEventHandler(createFilesBUT_Click); //创建文件夹系列
 
         }
 
         //鼠标移动
         void DragArea_mouseDown(object sender, MouseButtonEventArgs e)
         {
-            this.DragMove(); 
+            this.DragMove();
         }
         ///关闭程序
         private void exitButton_Click(object sender, RoutedEventArgs e)
@@ -44,17 +45,50 @@ namespace XuygTools
         ///获取路径
         private void getPathBUT_Click(object sender, RoutedEventArgs e)
         {
-             var  folderBrowserDialog1= new System.Windows.Forms.FolderBrowserDialog();
+            var folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 this.filePathForProject.Text = folderBrowserDialog1.SelectedPath;
             }
-            
-        }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        }
+        ///创建文件夹系统
+        private void createFilesBUT_Click(object sender, RoutedEventArgs e)
         {
+            string[] configText = File.ReadAllLines(@"C:\Users\Administrator\Source\Repos\xuygtools\XuygTools\PIC\config.txt");//获取项目根目录
+            var projectFileInList = new List<string>();
+            var isBegin = false;
+            for (int s = 0; s <= configText.Length - 1; s++)
+            {
+                if (configText[s] == @"KEY_WORD  PROJECT_FILE_IN_LIST_END")
+                { isBegin = false; }
+                if (isBegin)
+                { projectFileInList.Add(configText[s]); }
+                if (configText[s] == @"KEY_WORD PROJECT_FILE_IN_LIST_START")
+                { isBegin = true; }
+            }
 
+            var path = filePathForProject.Text + "\\" + this.projectName.Text;
+            if (!System.IO.Directory.Exists(path))
+            { System.IO.Directory.CreateDirectory(path); }
+            else
+            { System.Windows.Forms.MessageBox.Show("文件已经存在！"); }
+
+            for (int s = 0; s <= projectFileInList.Count-1 ; s++)
+            {
+                var pathNext = path + "\\" + projectFileInList[s];
+                if (!System.IO.Directory.Exists(pathNext))
+                {                    System.IO.Directory.CreateDirectory(pathNext);
+
+                }
+                else
+                {                    System.Windows.Forms.MessageBox.Show("文件已经存在！");                }
+               
+            }
+            System.Windows.Forms.MessageBox.Show("操作成功！");
         }
+
+
+
     }
 }
